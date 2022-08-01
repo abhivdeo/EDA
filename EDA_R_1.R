e6b7ffae1e6574536b0ef22d27f1b61e419a8362
Abhishek Deodhar <abhivdeo@gmail.com>
2022-08-01 23:26:24 +0530

    v1

---
+++ EDA_R_1.R
@@ -0,0 +1,8 @@
+
+
+#DATA
+
+#datasource <- read.csv("https://stats.govt.nz/assets/Uploads/Annual-enterprise-survey/Annual-enterprise-survey-2021-financial-year-provisional/Download-data/annual-enterprise-survey-2021-financial-year-provisional-csv.csv")
+
+datasource <- read.csv("G:/IGREN Main Gdrive/Self/GITHUB/EDA/annual-enterprise-survey-2021-financial-year-provisional-csv.csv")
+summary(datasource)
